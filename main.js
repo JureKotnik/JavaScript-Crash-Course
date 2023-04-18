4e82fa7624c3c403da645cbe66320a37ec181061
JureKotnik <jure2002k@gmail.com>
2023-04-18 08:11:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// Log a statement
+//console.log('Hello from main.js');
+
+// Variables
+
+//let age = 21;
+//console.log(age);
+
+//const salary = 0;
+//salary = 80000;
+//console.log(salary);
+
+//let sum = 0
+//sum = 5;
+//console.log(sum)
